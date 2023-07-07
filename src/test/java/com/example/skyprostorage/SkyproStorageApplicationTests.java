aee8ad69730560e303d2168967858b4914aa0dd9
IvanCaravan <ivan.kirsanov.2000@mail.ru>
2023-07-07 18:40:51 +0300

    initial commit

---
+++ src/test/java/com/example/skyprostorage/SkyproStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprostorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
